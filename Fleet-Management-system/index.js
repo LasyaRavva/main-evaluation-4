9c73754c8a9ad90609a6ac4fe15d2c9337a8bcb3
lasyaravva <LasyaRavva>
2026-01-31 21:12:38 +0530

    main app setup DONE

--- Fleet-Management-system/index.js
+++ Fleet-Management-system/index.js
@@ -5,6 +5,7 @@ const notFound = require('./middlewares/notFound');
 const userRoutes = require('./routes/userRoutes');
 const vehicleRoutes = require('./routes/vehicleRoutes');
 const tripRoutes = require('./routes/tripRoutes');
+const { not } = require('joi');
 const analyticsRoutes = requires('./routes/analyticsRoutes.js');
 
 const app = express();
@@ -12,3 +13,11 @@ app.use(express.json());
 app.use(logger);
 
 app.use('/users', userRoutes);
+app.use('/vehicles', vehicleRoutes);
+app.use('/trips', tripRoutes);
+app.use('/analytics', analyticsRoutes);
+
+app.use(notFound)
+
+const PORT = process.env.PORT ||3000;
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
